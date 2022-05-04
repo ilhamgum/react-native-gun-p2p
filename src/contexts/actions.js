cf98656c2cfa9b58fcd21aaa77b0678ab9c48092
ilhamgum <ilham.gumilang29@gmail.com>
2022-05-04 23:04:34 +0700

    initialize gun and create action context

--- src/contexts/actions.js
+++ src/contexts/actions.js
@@ -2,14 +2,36 @@ import useGun from "../hooks/useGun";
 const { user, SEA } = useGun();
 
 export async function authUser(dispatch, payload) {
+  user.auth(payload.key);
+  if (user.is) {
+    user
+      .get("profile")
+      .get("name")
+      .on((username) => {
+        // Update the username and key in our auth context
+        dispatch({
+          type: "AUTH",
+          payload: {
+            username: username,
+            key: JSON.stringify(user.is),
+          },
+        });
+      });
+  }
   console.log("dispatch", dispatch);
   console.log("payload", payload);
 }
 export async function createUser(dispatch, payload) {
+  var key = await SEA.pair();
+  authUser(dispatch, { key: key, username: payload.username });
+  user.get("profile").get("name").put(payload.username);
   console.log("dispatch", dispatch);
   console.log("payload", payload);
 }
 
 export async function logoutUser(dispatch) {
+  user.leave();
+  // Resets the username and key in our auth context
+  dispatch({ type: "AUTH_LOGOUT" });
   console.log("dispatch", dispatch);
 }
